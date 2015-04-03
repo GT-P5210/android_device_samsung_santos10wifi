c6339db7b67680b4fbfe085d271873130382fa28
Benoît Laniel <nels+git@pgroupe.net>
2015-04-03 22:23:09 +0200

    First import of device tree for santos10wifi

---
+++ BoardConfig.mk
@@ -0,0 +1,97 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+TARGET_ARCH := x86
+TARGET_ARCH_VARIANT := x86-atom
+TARGET_CPU_ABI := x86
+TARGET_CPU_SMP := true
+
+TARGET_BOARD_PLATFORM := clovertrail
+TARGET_BOOTLOADER_BOARD_NAME := santos10wifi
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1780373820
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# Use dlmalloc
+MALLOC_IMPL := dlmalloc
+
+# Kernel cmdline -- we'll see later, not taken into account by kernel for now
+#BOARD_KERNEL_CMDLINE :=
+
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+
+# Wifi
+BOARD_WLAN_DEVICE := bcmdhd
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/dhd.ko"
+WIFI_DRIVER_MODULE_NAME := "dhd"
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_AP := "/vendor/firmware/bcmdhd_apsta.bin_b2"
+WIFI_DRIVER_FW_PATH_STA := "/vendor/firmware/bcmdhd_sta.bin_b2"
+WIFI_DRIVER_FW_PATH_P2P := "/vendor/firmware/bcmdhd_p2p.bin_b2"
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/santos10wifi/bluetooth
+
+# Audio
+#BOARD_USES_ALSA_AUDIO := true
+#BOARD_USES_TINY_ALSA_AUDIO := true
+
+# DRM Protected Video
+#BOARD_WIDEVINE_OEMCRYPTO_LEVEL := 1
+#USE_INTEL_SECURE_AVC := true
+
+# enable ARM codegen for x86 with Houdini
+#BUILD_ARM_FOR_X86 := true
+
+#BOARD_SEPOLICY_DIRS := device/samsung/santos10wifi/sepolicy
+#BOARD_SEPOLICY_UNION := \
+#    bluetooth.te \
+#    btfwloader.te \
+#    dhcp.te \
+#    dumpstate.te \
+#    file.te \
+#    mediaserver.te \
+#    netd.te \
+#    property.te \
+#    pvrsrvctl.te \
+#    surfaceflinger.te \
+#    system_app.te \
+#    wpa.te \
+#    shell.te \
+#    sepfs.te \
+#    file_contexts \
+#    property_contexts
+
+# No opensource opengl renderer
+USE_OPENGL_RENDERER := false
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/santos10
+TARGET_KERNEL_CONFIG := santos10_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := x86_64-linux-android-
+BOARD_KERNEL_IMAGE_NAME := bzImage
+
+# Use the non-open-source parts, if they're present
+-include vendor/samsung/santos10wifi/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,198 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Need AppWidget permission to prevent from Launcher's crash.
+# TODO(pattjin): Remove this when the TV Launcher is used, which does not support AppWidget.
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.app_widgets.xml:system/etc/permissions/android.software.app_widgets.xml
+
+PRODUCT_AAPT_CONFIG := normal large xlarge hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=160
+
+# There may be a cleaner way to do this.
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=96m \
+    dalvik.vm.heapsize=256m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=2m
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    panel.physicalWidthmm = 217 \
+    panel.physicalHeightmm = 136 \
+    gsm.net.interface = rmnet0 \
+    persist.system.at-proxy.mode = 0 \
+    persist.ril-daemon.disable = 0 \
+    persist.radio.ril_modem_state = 1
+
+# ---- stock ----
+#PRODUCT_PROPERTY_OVERRIDES += \
+#+ro.build.scafe=americano
+#+ro.build.scafe.size=grande
+#+ro.com.google.clientidbase=android-samsung
+#+ro.com.google.gmsversion=4.4.2_r2
+#+ro.config.media_sound=Media_preview_Touch_the_light.ogg
+#+ro.config.personality=compat_layout
+#+ro.error.receiver.default=com.samsung.receiver.error
+#+ro.hdcp2.rx=tz
+#+ro.intel.corp.email=1
+#+ro.product.cpu.abi2=armeabi-v7a
+#+ro.ril.gprsclass=10
+#+ro.ril.hsxpa=1
+#+ro.ril.status.polling.enable=0
+#+ro.sec.fle.encryption=true
+#+ro.secwvk=144
+#+ro.streaming.video.drs=true
+#+rs.gpu.filterscript=0
+#+rs.gpu.renderscript=0
+#+rs.gpu.rsIntrinsic=0
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+DEVICE_PACKAGE_OVERLAYS := \
+    device/samsung/santos10wifi/overlay
+
+PRODUCT_COPY_FILES += \
+    device/samsung/santos10wifi/rootdir/fstab.santos10wifi:root/fstab.santos10wifi \
+    device/samsung/santos10wifi/rootdir/init.avc.rc:root/init.avc.rc \
+    device/samsung/santos10wifi/rootdir/init.common.rc:root/init.common.rc \
+    device/samsung/santos10wifi/rootdir/init.debug.rc:root/init.debug.rc \
+    device/samsung/santos10wifi/rootdir/init.diag.rc:root/init.diag.rc \
+    device/samsung/santos10wifi/rootdir/init.gps.cpd.rc:root/init.gps.cpd.rc \
+    device/samsung/santos10wifi/rootdir/init.gps.rc:root/init.gps.rc \
+    device/samsung/santos10wifi/rootdir/init.partlink.rc:root/init.partlink.rc \
+    device/samsung/santos10wifi/rootdir/init.platform.usb.rc:root/init.platform.usb.rc \
+    device/samsung/santos10wifi/rootdir/init.santos10wifi.rc:root/init.santos10wifi.rc \
+    device/samsung/santos10wifi/rootdir/init.watchdog.rc:root/init.watchdog.rc \
+    device/samsung/santos10wifi/rootdir/init.wifi.rc:root/init.wifi.rc \
+    device/samsung/santos10wifi/rootdir/init.wifi.vendor.rc:root/init.wifi.vendor.rc \
+    device/samsung/santos10wifi/rootdir/init.wireless.rc:root/init.wireless.rc \
+    device/samsung/santos10wifi/rootdir/props.rc:root/props.rc \
+    device/samsung/santos10wifi/rootdir/props.board.rc:root/props.board.rc \
+    device/samsung/santos10wifi/rootdir/props.platform.rc:root/props.platform.rc \
+    device/samsung/santos10wifi/rootdir/ueventd.santos10wifi.rc:root/ueventd.santos10wifi.rc \
+    \
+    device/samsung/santos10wifi/vendor/partlink:root/sbin/partlink \
+    device/samsung/santos10wifi/vendor/ia_watchdogd:root/usr/bin/ia_watchdogd
+
+PRODUCT_PACKAGES += \
+    libGLES_android
+
+# Audio
+#PRODUCT_PACKAGES += \
+#    libtinyalsa \
+#    audio.primary.fugu
+
+#USE_CUSTOM_AUDIO_POLICY := 1
+
+# specific management of audio_policy.conf
+#PRODUCT_COPY_FILES += \
+#    device/asus/fugu/audio_policy.conf:system/etc/audio_policy.conf
+
+# Hdmi CEC: Fugu works as a playback device (4).
+#PRODUCT_PROPERTY_OVERRIDES += ro.hdmi.device_type=4
+
+# Boot Animation
+#PRODUCT_COPY_FILES += \
+#    device/asus/fugu/bootanimation.zip:system/media/bootanimation.zip
+
+# Bluetooth
+#PRODUCT_PACKAGES += \
+#    bt_bcm4354
+
+#PRODUCT_COPY_FILES += \
+#    device/asus/fugu/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
+#Video
+PRODUCT_COPY_FILES += \
+    device/samsung/santos10wifi/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    device/samsung/santos10wifi/configs/wrs_omxil_components.list:system/etc/wrs_omxil_components.list \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    device/samsung/santos10wifi/configs/media_codecs.xml:system/etc/media_codecs.xml
+#### ??? ####
+#    device/asus/fugu/vp9_interpredict.binary:system/etc/vp9_interpredict.binary \
+#    device/asus/fugu/video_isv_profile.xml:system/etc/video_isv_profile.xml
+
+# Wifi
+PRODUCT_PACKAGES += \
+    libwpa_client \
+    lib_driver_cmd_bcmdhd \
+    hostapd \
+    dhcpcd.conf \
+    wpa_supplicant
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+#### Add it later
+#    frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml \
+
+#### ??? ####
+#    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+#    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+#    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
+#    frameworks/native/data/etc/android.hardware.hdmi.cec.xml:system/etc/permissions/android.hardware.hdmi.cec.xml
+
+# Key layout files
+PRODUCT_COPY_FILES += \
+    device/samsung/santos10wifi/prebuilts/sec_keyboard.idc:system/usr/idc/sec_keyboard.idc \
+    \
+    device/samsung/santos10wifi/prebuilts/sec_key.kl:system/usr/keylayout/sec_key.kl
+
+# Thermal itux
+#ENABLE_ITUXD := true
+#PRODUCT_PACKAGES += \
+#    ituxd
+
+# Power HAL
+PRODUCT_PACKAGES += \
+    power.santos10wifi
+
+#$(call inherit-product-if-exists, vendor/intel/PRIVATE/fugu/device-vendor.mk)
+#$(call inherit-product-if-exists, vendor/intel/moorefield/prebuilts/houdini/houdini.mk)
+
+# Add WiFi Firmware
+#$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4354/device-bcm.mk)
+
+# specific management of sep_policy.conf
+#PRODUCT_COPY_FILES += \
+#    device/asus/fugu/sep_policy.conf:system/etc/security/sep_policy.conf
+
+PRODUCT_CHARACTERISTICS := tablet
+
+$(call inherit-product-if-exists, vendor/samsung/santos10wifi/device-vendor.mk)
