96e3102f0a8d4bcc761ed12e01aab397b3af7346
Benoît Laniel <nels+git@pgroupe.net>
2015-04-06 20:48:01 +0200

    Adjust liblights to santos10

--- liblights/lights.c
+++ liblights/lights.c
@@ -42,30 +42,7 @@ static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 char const*const PANEL_FILE = "/sys/class/backlight/panel/brightness";
 char const*const BUTTON_FILE = "/sys/class/sec/sec_touchkey/brightness";
 
-char const*const LED_RED = "/sys/class/sec/led/led_r";
-char const*const LED_GREEN = "/sys/class/sec/led/led_g";
-char const*const LED_BLUE = "/sys/class/sec/led/led_b";
-char const*const LED_BLINK = "/sys/class/sec/led/led_blink";
-char const*const LED_BRIGHTNESS = "/sys/class/sec/led/led_br_lev";
-
 #define MAX_WRITE_CMD 25
-
-struct led_config {
-    int red;
-    int green;
-    int blue;
-    char blink[MAX_WRITE_CMD];
-};
-
-static struct light_state_t g_BatteryStore;
-static struct light_state_t g_AttentionStore;
-static struct light_state_t g_NotificationStore;
-
-static int
-is_lit(struct light_state_t const* state)
-{
-    return state->color & 0x00ffffff;
-}
 
 void init_g_lock(void)
 {
@@ -184,83 +161,6 @@ set_light_buttons(struct light_device_t* dev,
     return err;
 }
 
-/* LEDs */
-static int write_leds(struct led_config led)
-{
-    int err = 0;
-    pthread_mutex_lock(&g_lock);
-    err = write_int(LED_RED, led.red);
-    err = write_int(LED_GREEN, led.green);
-    err = write_int(LED_BLUE, led.blue);
-    err = write_str(LED_BLINK, led.blink);
-    pthread_mutex_unlock(&g_lock);
-    return err;
-}
-
-static int set_light_leds(struct light_state_t const *state)
-{
-    struct led_config led;
-    struct light_state_t *activeState;
-    int onMS, offMS;
-    unsigned int colorRGB;
-
-    if (is_lit(&g_AttentionStore)) {
-        activeState = &g_AttentionStore;
-        colorRGB = get_dimmed_color(activeState, 200);
-    } else {
-        if (is_lit(&g_BatteryStore) && !is_lit(&g_NotificationStore)) {
-            activeState = &g_BatteryStore;
-            colorRGB = get_dimmed_color(activeState, 20);
-        } else {
-            activeState = &g_NotificationStore;
-            colorRGB = get_dimmed_color(activeState, 200);
-        }
-    }
-
-
-    switch (activeState->flashMode) {
-    case LIGHT_FLASH_TIMED:
-        onMS = activeState->flashOnMS;
-        offMS = activeState->flashOffMS;
-        break;
-    case LIGHT_FLASH_HARDWARE:
-    case LIGHT_FLASH_NONE:
-    default:
-        onMS = 0;
-        offMS = 0;
-        break;
-    }
-
-    led.red = (colorRGB >> 16) & 0xFF;
-    led.green = (colorRGB >> 8) & 0xFF;
-    led.blue = colorRGB & 0xFF;
-    snprintf(led.blink, MAX_WRITE_CMD, "0x%x %d %d", colorRGB, onMS, offMS);
-    ALOGD("set_light_leds 0x%x %d %d", colorRGB, onMS, offMS);
-
-    return write_leds(led);
-}
-
-static int set_light_leds_notifications(struct light_device_t *dev,
-            struct light_state_t const *state)
-{
-    g_NotificationStore = *state;
-    return set_light_leds(state);
-}
-
-static int set_light_battery(struct light_device_t *dev,
-            struct light_state_t const *state)
-{
-    g_BatteryStore = *state;
-    return set_light_leds(state);
-}
-
-static int set_light_leds_attention(struct light_device_t *dev,
-            struct light_state_t const *state)
-{
-    g_AttentionStore = *state;
-    return set_light_leds(state);
-}
-
 static int close_lights(struct light_device_t *dev)
 {
     ALOGV("close_light is called");
@@ -278,18 +178,8 @@ static int open_lights(const struct hw_module_t *module, char const *name,
 
     if (0 == strcmp(LIGHT_ID_BACKLIGHT, name))
         set_light = set_light_backlight;
-#if !defined(EXYNOS4X12_TABLET) || defined(EXYNOS4X12_TABLET_HAS_LED_BUTTONS)
     else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
         set_light = set_light_buttons;
-#endif
-#ifndef EXYNOS4X12_TABLET
-    else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
-        set_light = set_light_leds_notifications;
-    else if (0 == strcmp(LIGHT_ID_ATTENTION, name))
-        set_light = set_light_leds_attention;
-    else if (0 == strcmp(LIGHT_ID_BATTERY, name))
-        set_light = set_light_battery;
-#endif
     else
         return -EINVAL;
 
@@ -318,7 +208,7 @@ struct hw_module_t HAL_MODULE_INFO_SYM = {
     .version_major = 1,
     .version_minor = 0,
     .id = LIGHTS_HARDWARE_MODULE_ID,
-    .name = "Exynos4x12 lights Module",
+    .name = "Santos10 lights Module",
     .author = "The CyanogenMod Project",
     .methods = &lights_module_methods,
 };
