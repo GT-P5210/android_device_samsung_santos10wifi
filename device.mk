e97b4f5db125a76ac1fae42d22045a153458797b
Benoît Laniel <nels+git@pgroupe.net>
2015-04-07 20:33:24 +0200

    Switch to Launcher3 and cleanup makefile

--- device.mk
+++ device.mk
@@ -22,6 +22,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_AAPT_CONFIG := normal large xlarge hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
+PRODUCT_CHARACTERISTICS := tablet
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=160
 
@@ -42,34 +44,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.ril-daemon.disable = 0 \
     persist.radio.ril_modem_state = 1
 
-# ---- stock ----
-#PRODUCT_PROPERTY_OVERRIDES += \
-#+ro.build.scafe=americano
-#+ro.build.scafe.size=grande
-#+ro.com.google.clientidbase=android-samsung
-#+ro.com.google.gmsversion=4.4.2_r2
-#+ro.config.media_sound=Media_preview_Touch_the_light.ogg
-#+ro.config.personality=compat_layout
-#+ro.error.receiver.default=com.samsung.receiver.error
-#+ro.hdcp2.rx=tz
-#+ro.intel.corp.email=1
-#+ro.product.cpu.abi2=armeabi-v7a
-#+ro.ril.gprsclass=10
-#+ro.ril.hsxpa=1
-#+ro.ril.status.polling.enable=0
-#+ro.sec.fle.encryption=true
-#+ro.secwvk=144
-#+ro.streaming.video.drs=true
-#+rs.gpu.filterscript=0
-#+rs.gpu.renderscript=0
-#+rs.gpu.rsIntrinsic=0
-
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
 DEVICE_PACKAGE_OVERLAYS := \
     device/samsung/santos10wifi/overlay
 
+# Boot files (needs work)
 PRODUCT_COPY_FILES += \
     device/samsung/santos10wifi/rootdir/fstab.santos10wifi:root/fstab.santos10wifi \
     device/samsung/santos10wifi/rootdir/init.avc.rc:root/init.avc.rc \
@@ -92,52 +73,21 @@ PRODUCT_COPY_FILES += \
     \
     device/samsung/santos10wifi/vendor/partlink:root/sbin/partlink
 
+# Custom packages
 PRODUCT_PACKAGES += \
-    libGLES_android
+    Launcher3
 
-# Lights
+# Lights HAL
 PRODUCT_PACKAGES += \
     lights.santos10wifi
 
-# IR
+# IR HAL
 PRODUCT_PACKAGES += \
     consumerir.santos10wifi
 
-# Audio
-#PRODUCT_PACKAGES += \
-#    libtinyalsa \
-#    audio.primary.fugu
-
-#USE_CUSTOM_AUDIO_POLICY := 1
-
-# specific management of audio_policy.conf
-#PRODUCT_COPY_FILES += \
-#    device/asus/fugu/audio_policy.conf:system/etc/audio_policy.conf
-
-# Hdmi CEC: Fugu works as a playback device (4).
-#PRODUCT_PROPERTY_OVERRIDES += ro.hdmi.device_type=4
-
-# Boot Animation
-#PRODUCT_COPY_FILES += \
-#    device/asus/fugu/bootanimation.zip:system/media/bootanimation.zip
-
-# Bluetooth
-#PRODUCT_PACKAGES += \
-#    bt_bcm4354
-
-#PRODUCT_COPY_FILES += \
-#    device/asus/fugu/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
-
-#Video
-PRODUCT_COPY_FILES += \
-    device/samsung/santos10wifi/configs/media_profiles.xml:system/etc/media_profiles.xml \
-    device/samsung/santos10wifi/configs/wrs_omxil_components.list:system/etc/wrs_omxil_components.list \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    device/samsung/santos10wifi/configs/media_codecs.xml:system/etc/media_codecs.xml
-#### ??? ####
-#    device/asus/fugu/vp9_interpredict.binary:system/etc/vp9_interpredict.binary \
-#    device/asus/fugu/video_isv_profile.xml:system/etc/video_isv_profile.xml
+# Power HAL
+PRODUCT_PACKAGES += \
+    power.santos10wifi
 
 # Wifi
 PRODUCT_PACKAGES += \
@@ -147,6 +97,15 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+# Video configuration
+PRODUCT_COPY_FILES += \
+    device/samsung/santos10wifi/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    device/samsung/santos10wifi/configs/wrs_omxil_components.list:system/etc/wrs_omxil_components.list \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    device/samsung/santos10wifi/configs/media_codecs.xml:system/etc/media_codecs.xml
+
+# Device configuration
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
@@ -169,38 +128,5 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
-#### ??? ####
-#    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
-#    frameworks/native/data/etc/android.hardware.hdmi.cec.xml:system/etc/permissions/android.hardware.hdmi.cec.xml
-
-# Key layout files
-PRODUCT_COPY_FILES += \
-    device/samsung/santos10wifi/prebuilts/sec_keyboard.idc:system/usr/idc/sec_keyboard.idc \
-    \
-    device/samsung/santos10wifi/prebuilts/sec_key.kl:system/usr/keylayout/sec_key.kl \
-    device/samsung/santos10wifi/prebuilts/sec_touchscreen.kl:system/usr/keylayout/sec_touchscreen.kl \
-    \
-    device/samsung/santos10wifi/configs/excluded-input-devices.xml:system/etc/excluded-input-devices.xml
-
-# Thermal itux
-#ENABLE_ITUXD := true
-#PRODUCT_PACKAGES += \
-#    ituxd
-
-# Power HAL
-PRODUCT_PACKAGES += \
-    power.santos10wifi
-
-#$(call inherit-product-if-exists, vendor/intel/PRIVATE/fugu/device-vendor.mk)
-#$(call inherit-product-if-exists, vendor/intel/moorefield/prebuilts/houdini/houdini.mk)
-
-# Add WiFi Firmware
-#$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4354/device-bcm.mk)
-
-# specific management of sep_policy.conf
-#PRODUCT_COPY_FILES += \
-#    device/asus/fugu/sep_policy.conf:system/etc/security/sep_policy.conf
-
-PRODUCT_CHARACTERISTICS := tablet
-
+# Use the non-open-source parts, if they're present
 $(call inherit-product-if-exists, vendor/samsung/santos10wifi/device-vendor.mk)
